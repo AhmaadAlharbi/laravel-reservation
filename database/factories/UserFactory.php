458824935d91f8a0f7ebb822c0379a656482cf65
AhmadAlharbi <ahmaadzaid7@gmail.com>
2023-11-27 09:58:49 +0300

    fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -46,4 +46,10 @@ class UserFactory extends Factory
             'role_id' => Role::ADMINISTRATOR->value,
         ]);
     }
+    public function guide(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role_id' => Role::GUIDE->value,
+        ]);
+    }
 }
